8e60ff766b8a84203a67e29435c690f6e26d061c
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2019-11-15 12:57:47 -0300

    Update printing.blade.php

--- resources/views/printings/partials/printing.blade.php
+++ resources/views/printings/partials/printing.blade.php
@@ -6,7 +6,17 @@
 <td>{{ \Carbon\Carbon::CreateFromFormat('Y-m-d H:i:s', $printing->created_at)->format('d/m/Y H:i') }} </td>
 <td>{{ $printing->pages }}</td>
 <td>{{ $printing->copies }}</td>
-<td>@php echo substr(explode(' ', $printing->filename, 2)[1],0,28);  @endphp...</td>
+<td>
+@php 
+if (strpos($printing->filename, 'smbprn.') !== false) {
+  $filename = substr(explode(' ', $printing->filename, 2)[1],0,28);
+}
+else {
+  $filename = substr($printing->filename,0,28);                    
+}
+echo utf8_decode($filename) . " ...";
+@endphp
+</td>
 
 @can('admin')
 <td><a href="/printings/{{ $printing->printer }}">{{ $printing->printer }}</a></td>
