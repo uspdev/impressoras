20c32efe5369e1a495cefc05b49f8c859a99cf4a
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-02-19 13:11:28 -0300

    colect host

---
+++ database/migrations/2020_02_19_130819_change_status_printings.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class ChangeStatusPrintings extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('printings', function (Blueprint $table) {
+            DB::statement("ALTER TABLE printings MODIFY status ENUM('Cancelado', 'Impresso', 'Processando','Fila','Problema')");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('printings', function (Blueprint $table) {
+            //
+        });
+    }
+}
