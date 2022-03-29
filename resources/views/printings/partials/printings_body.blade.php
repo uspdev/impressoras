183bc57c1fd091cba44ad1ab84e5a0391ca6eafb
Saotome <saotome@usp.br>
2022-03-28 22:14:22 -0300

    Resolvendo conflito com o arquivo app/Models/Printing

--- resources/views/printings/partials/printings_body.blade.php
+++ resources/views/printings/partials/printings_body.blade.php
@@ -11,3 +11,6 @@
 <td>{{ round((float)$printing->filesize/1024) }} MB</td>
 <td>{{ $printing->filename }}</td>
 <td>{{ $printing->latest_status ?? '' }}</td>
+@if (!$auth)
+<td>{{ $printing->authorizedByUserId->name ?? '' }}</td>
+@endif
