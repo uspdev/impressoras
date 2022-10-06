bea074ca2a5631ca18e19d3788464e93103c32ab
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2022-10-05 23:33:07 -0300

    acertando unidade

--- resources/views/printings/partials/printings_body.blade.php
+++ resources/views/printings/partials/printings_body.blade.php
@@ -5,7 +5,7 @@
 <td>{{ \Carbon\Carbon::CreateFromFormat('Y-m-d H:i:s', $printing->created_at)->format('d/m/Y H:i') }} </td>
 <td>{{ $printing->pages }}</td>
 <td>{{ $printing->copies }}</td>
-<td>{{ round((float)$printing->filesize/1024) }} MB</td>
+<td>{{ round((float) $printing->filesize/1024) }} KB</td>
 <td>{{ $printing->filename }}</td>
 <td>{{ $printing->latest_status ?? '' }}</td>
 @if (!empty($auth))
