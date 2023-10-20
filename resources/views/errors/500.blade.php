8fe7680bd7f4ae385ad025a6cc144527eacf197b
Will <wgnann@gmail.com>
2023-10-20 18:49:32 -0300

    timeout de 45s para impressão e mensagem de erro

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,32 @@
+@extends('master')
+
+@section('code', '500')
+
+@section('title', 'Erro ao imprimir.')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <div class="alert alert-danger">
+                    <h1>500: erro no servidor!</h1>
+                </div>
+                <div class="lead">
+                    <p>Seu trabalho demorou tempo demais para ser processado.</p>
+                    <p>Pode haver alguma imagem muito grande embutida no PDF.</p>
+                </div>
+            </div>
+        </div>
+        <div class="card-header">
+            <h4>O que fazer?</h4>
+        </div>
+        <div class="card">
+            <ol>
+                <li>Abrir o documento no leitor de PDF (e.g. Okular);</li>
+                <li>Mandar imprimir <b>para arquivo</b>;</li>
+                <li>Marcar a opção <b>Forçar a rasterização</b>;</li>
+                <li>Enviar o arquivo "reimpresso".</li>
+            </ol>
+        </div>
+    </div>
+@endsection
