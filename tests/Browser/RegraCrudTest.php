0f105a5855e23690f0832427fb9034afa1d92076
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2026-07-03 10:38:08 -0300

    Implementando Test Crud Regras

--- tests/Browser/RegraCrudTest.php
+++ tests/Browser/RegraCrudTest.php
@@ -9,9 +9,9 @@ use Tests\DuskTestCase;
 class RegraCrudTest extends DuskTestCase
 {
     /**
-     * A Dusk test example.
+     * Método para fazer o Crud Completo
      */
-    public function test_login()
+    public function test_crud_regras()
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
@@ -20,7 +20,9 @@ class RegraCrudTest extends DuskTestCase
                 ->typeSlowly('loginUsuario', '1111')
                 ->press('Login')
                 ->waitFor('.login_logout_link') # Importante: Espera a página de retornada carregar
-                ->assertSee('Regras');
+                ->assertSee('Regras')
+                # Alan vai Implementat CRUD de regras
+                ->clickLink('Regras');
         });
     }
 }
